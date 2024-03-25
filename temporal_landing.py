9b670332120529004ade5c4c4191761ac2c70228
darryl-abraham <darryl.br@gmail.com>
2024-03-25 19:11:13 +0100

    changed from overwrite files to check and skip if file already exists in HDFS, also added comments to make everything easier to understand

--- temporal_landing.py
+++ temporal_landing.py
@@ -1,32 +1,59 @@
 from hdfs import InsecureClient
 import os
 
-class HadoopManager:
+class DataCollector:
+    """
+    Data collector class to collect data from any source directory and store in HDFS
+    """
     def __init__(self, namenode, port, user):
         self.namenode = namenode
         self.port = port
         self.user = user
         self.client = InsecureClient(url='http://'+self.namenode+':'+self.port, user=self.user)
 
-    def upload_file(self, local_path, hdfs_target_dir):
-        self.client.upload(hdfs_target_dir, local_path, overwrite=True)
-        print("Uploaded file: " + local_path.split('/')[-1] + " to HDFS: " + hdfs_target_dir)
+    def upload_file(self, local_path, hdfs_path):
+        """
+        Upload a file to HDFS (irrespective of file extension)
+        :param local_path: path to local file
+        :param hdfs_path: target directory to upload to in HDFS
+        :return: None
+        """
+        if not self.client.status(hdfs_path + '/' + local_path.split('\\')[-1], strict=False):
+            self.client.upload(hdfs_path, local_path, overwrite=True)
+            print(f"Uploaded file: {local_path.split('/')[-1]} to HDFS: {hdfs_path}")
+        else:
+            print(f"File {local_path.split('/')[-1]} already exists in HDFS: {hdfs_path}")
 
     def upload_folder(self, local_dir, hdfs_target_dir):
+        """
+        Upload a data folder to HDFS
+        :param local_dir: folder path to data
+        :param hdfs_target_dir: target directory to upload to in HDFS
+        :return: None
+        """
         for item in os.listdir(local_dir):
             if os.path.isdir(os.path.join(local_dir, item)):
-                self.client.makedirs(hdfs_target_dir + "/" + item)
-                print("Created folder: " + item + " in HDFS: " + hdfs_target_dir)
+                if not self.client.status(hdfs_target_dir + "/" + item, strict=False):
+                    self.client.makedirs(hdfs_target_dir + "/" + item)
+                    print(f"Created folder: {item} in HDFS: {hdfs_target_dir}")
+                else:
+                    print(f"Folder {item} already exists in HDFS: {hdfs_target_dir}")
                 self.upload_folder(os.path.join(local_dir, item), hdfs_target_dir + "/" + item)
             elif os.path.isfile(os.path.join(local_dir, item)):
                 self.upload_file(os.path.join(local_dir, item), hdfs_target_dir)
 
-    def nuke(self, hdfs_target_dir='/user'):
+    def nuke(self, hdfs_target_dir='/bdm'):
+        """
+        Delete all
+        :param hdfs_target_dir: directory to delete
+        :return: None
+        """
         self.client.delete(hdfs_target_dir, recursive=True)
-        if hdfs_target_dir == '/user':
-            self.client.makedirs('/user')
+        if hdfs_target_dir == '/bdm':
             self.client.makedirs('/user/bdm')
+        print(f"Directory {hdfs_target_dir} nuked")
 
 if __name__ == '__main__':
-    hadoop_manager = HadoopManager(namenode='10.4.41.45', port='9870', user='bdm')
-    hadoop_manager.upload_folder(r'.\data', r'/user/bdm')
+    data_collector = DataCollector(namenode='10.4.41.45', port='9870', user='bdm')
+    data_collector.upload_folder(r'.\data', r'/user/bdm')
+    #hadoop_manager.nuke()
